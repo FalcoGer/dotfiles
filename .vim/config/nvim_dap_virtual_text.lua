63ea7c15514517baf23ec7edcae15fc196cd434d
FalcoGer <16250196+FalcoGer@users.noreply.github.com>
2023-11-29 06:13:53 +0100

    update dap-virtual text
    highlight all frames
    highlight new as changed
    link highlight to DiagnosticVirtualTextHint

--- .vim/config/nvim_dap_virtual_text.lua
+++ .vim/config/nvim_dap_virtual_text.lua
@@ -4,7 +4,7 @@ local options = {
     enabled = true,                        -- enable this plugin (the default)
     enabled_commands = true,               -- create commands DapVirtualTextEnable, DapVirtualTextDisable, DapVirtualTextToggle, (DapVirtualTextForceRefresh for refreshing when debug adapter did not notify its termination)
     highlight_changed_variables = true,    -- highlight changed values with NvimDapVirtualTextChanged, else always NvimDapVirtualText
-    highlight_new_as_changed = false,      -- highlight new variables in the same way as changed variables (if highlight_changed_variables)
+    highlight_new_as_changed = true,       -- highlight new variables in the same way as changed variables (if highlight_changed_variables)
     show_stop_reason = true,               -- show stop reason when stopped for exceptions
     commented = false,                     -- prefix virtual text with comment string
     only_first_definition = false,         -- only show virtual text at first definition (if there are multiple)
@@ -28,10 +28,18 @@ local options = {
     virt_text_pos = vim.fn.has 'nvim-0.10' == 1 and 'inline' or 'eol',
 
     -- experimental features:
-    all_frames = false,                    -- show virtual text for all stack frames not only current. Only works for debugpy on my machine.
+    all_frames = true,                    -- show virtual text for all stack frames not only current. Only works for debugpy on my machine.
     virt_lines = false,                    -- show virtual lines instead of virtual text (will flicker!)
     virt_text_win_col = nil                -- position the virtual text at a fixed window column (starting from the first text column) ,
                                            -- e.g. 80 to position at column 80, see `:h nvim_buf_set_extmark()`
 }
 
 require("nvim-dap-virtual-text").setup(options)
+
+-- Highlights
+-- NvimDapVirtualText               Used for inline hints of values, default links to "comment"
+vim.cmd("highlight link NvimDapVirtualText DiagnosticVirtualTextHint")
+-- NvimDapVirtualTextChanged        Used for inline hints of changed values, default links to "DiagnosticVirtualTextWarn"
+-- NvimDapVirtualTextInfo           Used for ???, defaults to "DiagnosticVirtualTextInfo"
+-- NvimDapVirtualTextError          Used for stop reason, defaults to "DiagnosticVirtualTextError"
+
