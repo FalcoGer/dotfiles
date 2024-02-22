55d3b030ff98b296a3ed38288a0c0f4d0d93a8fe
FalcoGer <16250196+FalcoGer@users.noreply.github.com>
2024-02-22 05:52:31 +0100

    fix neorg keybinds

--- .vim/config/neorg.lua
+++ .vim/config/neorg.lua
@@ -3,23 +3,6 @@
 
 local neorg = require('neorg')
 
-local dirman = require('neorg').modules.get_module("core.dirman")
-
-CreateNote = function()
-    local dirman = require('neorg').modules.get_module("core.dirman")
-    local noteName = vim.fn.input("New Note: ")
-
-    if noteName ~= nil and noteName ~= "" then
-        dirman.create_file(noteName, "notes", {
-            no_open  = false, -- open file after creation?
-            force    = false, -- overwrite file if exists
-            metadata = {} -- key-value table for metadata fields
-        })
-    end
-end
-
-vim.keymap.set('n', '<Leader>N', ':lua CreateNote()<CR>', { silent = true, desc = "Neorg new note."})
-
 local options = {
     load = {
         ["core.defaults"] = {},  -- Loads default behaviour
@@ -27,15 +10,17 @@ local options = {
             config = {
                 folds = true,
                 icon_preset = "varied", -- "basic", "diamond", "varied"
-                code_block = {
-                    conceal = true,     -- hide @code and @end tags
-                    content_only = false, -- darken only content or also @code and @end tags
-                    padding = {
-                        left = 8,
-                        right = 0,
+                icons = {
+                    code_block = {
+                        conceal = true,     -- hide @code and @end tags
+                        content_only = false, -- darken only content or also @code and @end tags
+                        padding = {
+                            left = 8,
+                            right = 0,
+                        },
+                        spell_check = false,
+                        width = "fullwidth", -- "fullwidth" or "content"
                     },
-                    spell_check = false,
-                    width = "fullwidth", -- "fullwidth" or "content"
                 },
             }
         }, -- Adds pretty icons to your documents
@@ -47,29 +32,32 @@ local options = {
                 default_workspace = "notes",
             },
         },
-        ["core.integrations.telescope"] = {}
+        ["core.integrations.telescope"] = {},
+        ["core.looking-glass"] = {}, -- code block editing
+        ["core.keybinds"] = {
+            config = {
+                hook = function(keybinds)
+                    keybinds.unmap("norg", "n", keybinds.leader .. "nn")
+
+                    keybinds.map_event_to_mode("norg", {
+                        n = { -- Bind keys in normal mode
+                            { "<M-l>", "core.integrations.telescope.find_linkable" },
+                            { "<Leader>N", "core.dirman.new.note" },
+                            { "<C-CR>", "core.looking-glass.magnify-code-block" }
+                        },
+
+                        i = { -- Bind in insert mode
+                            { "<M-l>", "core.integrations.telescope.insert_link" },
+                        },
+                    }, {
+                        silent = true,
+                        noremap = true,
+                    })
+                end,
+            },
+        },
     },
 }
 
 neorg.setup(options)
 
-local neorg_callbacks = require("neorg.core.callbacks")
-
-neorg_callbacks.on_event(
-    "core.keybinds.events.enable_keybinds",
-    function(_, keybinds)
-        -- Map all the below keybinds only when the "norg" mode is active
-        keybinds.map_event_to_mode("norg", {
-            n = { -- Bind keys in normal mode
-                { "<M-s>", "core.integrations.telescope.find_linkable" },
-            },
-
-            i = { -- Bind in insert mode
-                { "<M-l>", "core.integrations.telescope.insert_link" },
-            },
-        }, {
-            silent = true,
-            noremap = true,
-        })
-    end
-)
