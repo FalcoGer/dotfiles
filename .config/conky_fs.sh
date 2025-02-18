135cc0a5895400123d33c444cda557786189c61f
FalcoGer <16250196+FalcoGer@users.noreply.github.com>
2025-02-18 08:59:46 +0100

    conky update
    - removes old comments
    - properly formats mount points
    - checks network states and better formatting for network

--- .config/conky_fs.sh
+++ .config/conky_fs.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
-awk '$3 ~ /ext4|btrfs|xfs|fat32|fat16|exfat|ntfs/ && $2 !~ /\/snap\// {print "${color1}" $2, "\t${color2}${fs_used " $2 "}/${fs_size " $2 "} $alignr${fs_used_perc " $2 "}% ${color}${fs_bar 8,100 " $2 "}"}' /proc/mounts
+
+# Declare an array to store mount points
+mount_points=()
+
+# Find the longest mount point name
+max_len=0
+
+# Read from /proc/mounts, update mount_points array, and update max_len 
+while read -r device mount fs_type rest; do
+    if [[ "$fs_type" =~ ^(ext2|ext3|ext4|btrfs|xfs|fat32|fat16|exfat|ntfs)$ ]] && [[ "$mount" != */snap/* ]]; then
+        mount_points+=("$mount")
+        (( ${#mount} > max_len )) && max_len=${#mount}
+    fi
+done < /proc/mounts
+
+# Print the formatted output
+for mount in "${mount_points[@]}"; do
+    printf "\${color1}%-${max_len}s  \${color2}\${fs_used %s}/\${fs_size %s} \$alignr\${fs_used_perc %s}%% \${color}\${fs_bar 8,100 %s}\n" \
+        "$mount" "$mount" "$mount" "$mount" "$mount"
+done
--- .config/conky_network.sh
+++ .config/conky_network.sh
@@ -2,14 +2,21 @@
 
 # Get list of network interfaces
 for interface in $(ls /sys/class/net/); do
-    # Exclude 'docker#', 'lo', and any interfaces containing 'bridge'
-    if [[ "$interface" != lo && "$interface" != docker* && "$interface" != veth* && "$interface" != *bridge* && 
-          -d "/sys/class/net/$interface" && -f "/sys/class/net/$interface/address" ]]; then
-        # Output Conky config for the interface
-        echo "\${if_up $interface}\${color}\${stippled_hr}"
-        echo "\${color3}$interface:    \${color2}\${addr $interface}"
-        echo "\${color1}Down: \${color2}\${downspeed $interface}\${alignr}\${color1}Up: \${color2}\${upspeed $interface}"
-        echo "\${color4}\${downspeedgraph $interface 24,140 00aa00 00ffff}\${alignr}\${upspeedgraph $interface 24,140 ff8800 ff4400}"
-        echo "\${endif}"
+  # Exclude 'docker#', 'lo', and any interfaces containing 'bridge'
+  if [[ "$interface" != lo && "$interface" != docker* && "$interface" != veth* && "$interface" != *bridge* && "$interface" != virbr* && 
+      -d "/sys/class/net/$interface" && -f "/sys/class/net/$interface/address" ]]; then
+    # Check if the interface is up, has a link, and address
+    if [[ -f "/sys/class/net/$interface/operstate" && $(cat /sys/class/net/$interface/operstate) == "up" ]]; then
+      # Output Conky config for the interface
+      echo "\${if_up $interface}\${color}\${stippled_hr}"
+      echo "\${color3}$interface:    \${color2}\${addr $interface}"
+      echo "\${color1}Down: \${color2}\${downspeed $interface}\${alignr}\${color1}Up: \${color2}\${upspeed $interface}"
+      echo "\${color4}\${downspeedgraph $interface 24,140 00aa00 00ffff}\${alignr}\${upspeedgraph $interface 24,140 ff8800 ff4400}"
+      echo -n "\${endif}"
+    else
+      echo "\${if_up $interface}\${color}\${stippled_hr}"
+      echo "\${color3}$interface:    \${color2}\${addr $interface}"
+      echo -n "\${endif}"
     fi
+  fi
 done
